f0418e2b253603e4e0f83b65cd4706d429cbcf12
Yevchenko Bohdan <98body@gmail.com>
2016-12-29 21:20:44 +0200

    Add files via upload

---
+++ school/school/ReportForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace school
+{
+    partial class ReportForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.marksReportBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.schoolDataSet = new school.schoolDataSet();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.marksReportTableAdapter = new school.schoolDataSetTableAdapters.marksReportTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.marksReportBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDataSet)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // marksReportBindingSource
+            // 
+            this.marksReportBindingSource.DataMember = "marksReport";
+            this.marksReportBindingSource.DataSource = this.schoolDataSet;
+            // 
+            // schoolDataSet
+            // 
+            this.schoolDataSet.DataSetName = "schoolDataSet";
+            this.schoolDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "marksReport2";
+            reportDataSource1.Value = this.marksReportBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "school.Report1.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.Size = new System.Drawing.Size(810, 341);
+            this.reportViewer1.TabIndex = 14;
+            // 
+            // marksReportTableAdapter
+            // 
+            this.marksReportTableAdapter.ClearBeforeFill = true;
+            // 
+            // ReportForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(810, 341);
+            this.Controls.Add(this.reportViewer1);
+            this.Name = "ReportForm";
+            this.Text = "Табель";
+            this.Load += new System.EventHandler(this.ReportForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.marksReportBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.schoolDataSet)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource marksReportBindingSource;
+        private schoolDataSet schoolDataSet;
+        private schoolDataSetTableAdapters.marksReportTableAdapter marksReportTableAdapter;
+    }
+}
---
+++ school/school/ReportForm.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Reporting.WinForms;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace school
+{
+    public partial class ReportForm : Form
+    {
+        private int child_id;
+        private string childName;
+        private decimal year;
+
+        public ReportForm(int child_id, decimal year, string childName)
+        {
+            InitializeComponent();
+            this.child_id = child_id;
+            this.year = year;
+            this.childName = childName;
+        }
+
+        private void ReportForm_Load(object sender, EventArgs e)
+        {
+            ReportParameterCollection reportParameters = new ReportParameterCollection();
+            reportParameters.Add(new ReportParameter("childName", childName));
+            reportParameters.Add(new ReportParameter("year", Convert.ToString(year)));
+            this.reportViewer1.LocalReport.SetParameters(reportParameters);
+            // TODO: This line of code loads data into the 'schoolDataSet.marksReport' table. You can move, or remove it, as needed.
+            this.marksReportTableAdapter.Fill(schoolDataSet.marksReport, year, child_id, "оценка");
+            this.reportViewer1.RefreshReport();
+        }
+    }
+}
